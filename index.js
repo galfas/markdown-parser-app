7ea1548e584bcc4da12de8bf63192620aac5d837
Marcelo Juan Saciloto <marcelo.saciloto@walmart.com>
2016-08-17 23:04:00 -0300

    inserting log

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Hapi = require('hapi');
+const Good = require('good')
 
 //Create a server with a host and port
 const server = new Hapi.Server();
@@ -19,12 +20,41 @@ server.route({
 	}
 })
 
-server.start((err) => {
+server.route({
+	method: 'GET',
+	path: '/{name}',
+	handler: function (request, reply){
+		reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+	}
+});
 
-	if (err) {
+server.register({
+	register: Good,
+	options:{
+		reporters:{
+			console:[{
+				module: 'good-squeeze',
+				name: 'Squeeze',
+				args: [{
+					response:'*',
+					log:'*'
+				}]
+			},{
+				module:'good-console'
+			}, 'stdout']
+		}
+	}
+}, (err) => {
+	if(err) {
 		throw err;
 	}
 
-	console.log('Server running at:', server.info.uri);
-})
+	server.start((err) => {
 
+		if (err) {
+			throw err;
+		}
+
+		server.log('Server running at:', server.info.uri);
+	})
+})
